bdc61fb62560cc02126793fc7046c2199e96a631
Jittapan Pluemsumran <foreverfk@stephack.com>
2017-04-28 14:14:32 +0700

    Properly defined snprintf for MSVC++

--- src/common/cbasetypes.h
+++ src/common/cbasetypes.h
@@ -229,7 +229,8 @@ typedef uintptr_t uintptr;
 #define strcasecmp			stricmp
 #define strncasecmp			strnicmp
 #define strncmpi			strnicmp
-#if defined(__BORLANDC__) || _MSC_VER < 1900
+// MSVC++ does not define snprintf in C++ mode.
+#if defined(__BORLANDC__) || (_MSC_VER < 1900 || defined(__cplusplus))
 #define snprintf			_snprintf
 #endif
 #if defined(_MSC_VER) && _MSC_VER < 1400
