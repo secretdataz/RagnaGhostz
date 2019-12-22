27c4d9edaf82c208275094205ea4a2da65c56010
Zell <lukasrmattos@hotmail.com>
2019-12-22 19:56:50 -0300

    no message

--- src/map/status.hpp
+++ src/map/status.hpp
@@ -22,7 +22,7 @@ struct status_change;
  * Changing this limit requires edits to refine_db.txt
  **/
 #ifdef RENEWAL
-#	define MAX_REFINE 15
+#	define MAX_REFINE 12
 #else
 #	define MAX_REFINE 10
 #endif
